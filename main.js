9c465b90a19e856000c763a495b79b1cdc13f8b7
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2022-01-12 18:55:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var speechRecognition= window.webkitSpeechRecognition;
+
+var recognition= new speechRecognition();
+
+function load(){
+    document.getElementById("textbox").innerHTML= "";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var content= event.results[0][0].transcript;
+    console.log(content);
+
+    document.getElementById("textbox").innerHTML= content;
+}
